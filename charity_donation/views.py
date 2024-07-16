a3fcec8239fbd7e1e7b85ff68b2e1156c7bb8a93
Agnieszka Bojarska <ag.bojarska@gmail.com>
2024-07-16 15:52:41 +0200

    dynamic statistics

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,10 +1,17 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+
+from charity_donation.models import Donation
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'base.html')
+        donations = Donation.objects.all()
+        donation_sum = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
+        institutions = donations.values('institution').distinct()
+        institution_count = institutions.count()
+        return render(request, 'base.html', {'quantity': donation_sum, 'institution': institution_count})
 
 
 class AddDonation(View):
