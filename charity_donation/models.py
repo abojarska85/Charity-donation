1adae460840102eacc565dabc3afcd7908ee5c78
Agnieszka Bojarska <ag.bojarska@gmail.com>
2024-07-16 14:23:34 +0200

    institution model

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -8,3 +8,16 @@ class Category(models.Model):
         return self.name
 
 
+class Institution(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    INSTITUTION_TYPE = (
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna')
+    )
+    type = models.IntegerField(choices=INSTITUTION_TYPE, default=1)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}, {self.type}"
