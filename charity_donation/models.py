0f2320c4fd2e4ef0f0be5371f7f5d56ca2b0f26c
Agnieszka Bojarska <ag.bojarska@gmail.com>
2024-07-16 15:06:07 +0200

    donation model

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -21,3 +22,20 @@ class Institution(models.Model):
 
     def __str__(self):
         return f"{self.name}, {self.description}, {self.type}"
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+
+    def __str__(self):
+        return f"{self.quantity} {self.institution}"
