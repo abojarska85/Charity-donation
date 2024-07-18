53c7d2ee40b23c88edf7ef8ba957e49206ec21e2
Agnieszka Bojarska <ag.bojarska@gmail.com>
2024-07-18 15:28:11 +0200

    add donation view, login changes

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import TemplateView
 
-from charity_donation.models import Donation, Institution
+from charity_donation.models import Donation, Institution, Category
 
 
 class LandingPageView(View):
@@ -19,9 +21,16 @@ class LandingPageView(View):
                                               'institution': institution_name})
 
 
-class AddDonation(View):
+class AddDonation(LoginRequiredMixin, TemplateView):
+    template_name = 'form.html'
     def get(self, request):
-        return render(request, 'form.html')
+        context = self.get_context_data()
+        return render(request, 'form.html', context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class LoginView(View):
